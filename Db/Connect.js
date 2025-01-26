5d23f4629bf9c66e0baf4724f214e285b98eb4f4
ewriq <102306313+ewriq@users.noreply.github.com>
2025-01-26 12:37:47 +0300

    post

--- Db/Connect.js
+++ Db/Connect.js
@@ -1,15 +1,24 @@
-const sqlite3 = require('sqlite3').verbose();
-
+const { Client } = require('pg');
 
 function conn() {
-  const db = new sqlite3.Database('db.db', (err) => {
+  const client = new Client({
+    user: 'neondb_owner',          // PostgreSQL kullanıcı adı
+    host: 'ep-gentle-meadow-a2206mla-pooler.eu-central-1.aws.neon.tech',  // PostgreSQL host
+    database: 'neondb',             // PostgreSQL veritabanı adı
+    password: 'npg_uVkG8QMbdKX5',   // PostgreSQL şifresi
+    port: 5432,                    // PostgreSQL portu (genellikle 5432)
+    ssl: { rejectUnauthorized: false }  // SSL bağlantısı için
+  });
+
+  client.connect((err) => {
     if (err) {
       console.error('Veritabanına bağlanırken hata oluştu: ' + err.message);
     } else {
       console.log('Veritabanına başarıyla bağlanıldı.');
     }
   });
-  return db;
+  
+  return client;
 }
 
 module.exports = conn;
--- Db/Insert.js
+++ Db/Insert.js
@@ -1,12 +1,13 @@
-function Insert(db, id, time, callback) {
-    const insertData = db.prepare('INSERT INTO log (id, time) VALUES (?, ?)');
-
-    insertData.run(id, time, (err) => {
-        if (err) {
-            return callback(err, null);  
-        }
-        return callback(null, 200); 
+function Insert(client, id, time, callback) {
+    const query = 'INSERT INTO log (id, time) VALUES ($1, $2)';
+    
+    client.query(query, [id, time], (err, res) => {
+      if (err) {
+        return callback(err, null);
+      }
+      return callback(null, 200);
     });
-}
-
-module.exports = Insert;
+  }
+  
+  module.exports = Insert;
+  
