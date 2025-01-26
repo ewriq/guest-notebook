0a8a93ce81061fd79fc56fb061735e0dac0754a5
ewriq <102306313+ewriq@users.noreply.github.com>
2025-01-26 17:31:31 +0300

    sum

--- api/Db/Connect.js
+++ api/Db/Connect.js
@@ -1,14 +1,7 @@
 const { Client } = require('pg');
 
 function conn() {
-  const client = new Client({
-    user: 'neondb_owner',
-    host: 'ep-gentle-meadow-a2206mla-pooler.eu-central-1.aws.neon.tech', 
-    database: 'neondb',             
-    password: 'npg_uVkG8QMbdKX5',   
-    port: 5432,                   
-    ssl: { rejectUnauthorized: false } 
-  });
+  
 
   client.connect((err) => {
     if (err) {
