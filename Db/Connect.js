c69b430f3dc50944571fa0efa205a5adb7e40c33
ewriq <102306313+ewriq@users.noreply.github.com>
2025-01-26 12:39:53 +0300

    Update Connect.js

--- Db/Connect.js
+++ Db/Connect.js
@@ -2,12 +2,12 @@ const { Client } = require('pg');
 
 function conn() {
   const client = new Client({
-    user: 'neondb_owner',          // PostgreSQL kullanıcı adı
-    host: 'ep-gentle-meadow-a2206mla-pooler.eu-central-1.aws.neon.tech',  // PostgreSQL host
-    database: 'neondb',             // PostgreSQL veritabanı adı
-    password: 'npg_uVkG8QMbdKX5',   // PostgreSQL şifresi
-    port: 5432,                    // PostgreSQL portu (genellikle 5432)
-    ssl: { rejectUnauthorized: false }  // SSL bağlantısı için
+    user: 'neondb_owner',
+    host: 'ep-gentle-meadow-a2206mla-pooler.eu-central-1.aws.neon.tech', 
+    database: 'neondb',             
+    password: 'npg_uVkG8QMbdKX5',   
+    port: 5432,                   
+    ssl: { rejectUnauthorized: false } 
   });
 
   client.connect((err) => {
